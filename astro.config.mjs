a0eb068d1cf9b402622c47188fa5fac4c15483b4
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-10-26 19:52:00 +0200

    feat: update integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
-import { defineConfig } from "astro/config";
-import chooseTechIntegration from "@choose-tech/astro";
+import { defineConfig } from "@choose-tech/astro/config";
 
 export default defineConfig({
-  integrations: [
-    chooseTechIntegration({
-      base: "/javascript-libraries",
-      title: "JS Libraries",
-      description: "Easily compare various JS/TS libraries from the community.",
-      color: "#F7DF1E",
-      repo: "choose-tech/javascript-libraries",
-      logoFilename: "logo.svg",
-    }),
-  ],
+  base: "/javascript-libraries",
+  title: "JS Libraries",
+  description: "Easily compare various JS/TS libraries from the community.",
+  color: "#F7DF1E",
+  repo: "choose-tech/javascript-libraries",
+  logoFilename: "logo.svg",
 });
